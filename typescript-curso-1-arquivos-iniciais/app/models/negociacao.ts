3658abd74139ad1fe9de67d7e43274ed344fd380
Renato Veneroso <rveneroso@gmail.com>
2022-06-26 18:31:40 -0300

    Implementação de programação defensiva para impedir alterações em objetos da classe Negociacao através de métodos do tipo nativo do objeto

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -4,13 +4,23 @@ export class Negociacao {
     // Simplificando a classe com o modificador readonly de forma a não ser mais necessária a criação
     // dos métodos get para os atributos cujos valores não podem ser modificados.
     constructor(
-        public readonly data: Date, 
+        // O modificador readonly no atributo data impede apenas que seja feita um atribuição direta
+        // ao atributo. Ele não impede que, uma vez que o valor do atributo seja obtido, seu valor
+        // seja alterado através de algum dos métodos disponíveis do objeto Date. Para isso, ele será
+        // tranformado novamente em private.
+        private readonly _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number) { }
 
-   
-
     get volume(): number {
         return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        // Para evitar modificações no objeto _data através dos métodos nativos da classe Date,
+        // um clone do objeto será criado e é esse clone que será retornado pelo método get. Assim,
+        // quaisquer alterações feitas no objeto retornado não terão efeito sobre o atributo data.
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -2,12 +2,24 @@
 export class Negociacao {
     // Simplificando a classe com o modificador readonly de forma a não ser mais necessária a criação
     // dos métodos get para os atributos cujos valores não podem ser modificados.
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(
+    // O modificador readonly no atributo data impede apenas que seja feita um atribuição direta
+    // ao atributo. Ele não impede que, uma vez que o valor do atributo seja obtido, seu valor
+    // seja alterado através de algum dos métodos disponíveis do objeto Date. Para isso, ele será
+    // tranformado novamente em private.
+    _data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
     get volume() {
         return this.quantidade * this.valor;
+    }
+    get data() {
+        // Para evitar modificações no objeto _data através dos métodos nativos da classe Date,
+        // um clone do objeto será criado e é esse clone que será retornado pelo método get. Assim,
+        // quaisquer alterações feitas no objeto retornado não terão efeito sobre o atributo data.
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
